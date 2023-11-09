056c801c1701ec58d844b17ea9ede9b49a658b2b
Kaike Matos <kaikematos50@gmail.com>
2023-11-09 09:19:58 -0400

    atlz funcuinamento

--- projects/Calculator/js/script.js
+++ projects/Calculator/js/script.js
@@ -1,17 +1,17 @@
 var primeiroconj = [];
 var segundoconj = [];
 var visualizacao = [];
-var sinal;
+var sinal = [];
+var resutado;
 
 var resu_visu = document.getElementById("resu_visu");
-var resu;
+
 var pontocontrol;
 
 
 function imprimir() {
 
     resu_visu.innerHTML = visualizacao;
-    console.log(resu);
 }
 
 
@@ -28,145 +28,243 @@ function porcent() {
 */
 
 function confirma() {
-    console.log(primeiroconj);
-    console.log(segundoconj);
+    switch (equacao) {
+        case 1:
+            resutado = parseFloat(primeiroconj.join()) / parseFloat(segundoconj.join());
+
+            visualizacao = resutado;
+            imprimir();
+            break;
+
+        case 2:
+            resutado = parseFloat(primeiroconj.join()) * parseFloat(segundoconj.join());
+
+            visualizacao = resutado;
+            imprimir();
+            break;
+
+        case 3:
+            resutado = parseFloat(primeiroconj.join()) - parseFloat(segundoconj.join());
+
+            visualizacao = resutado;
+            imprimir();
+            break;
+
+        case 4:
+            resutado = parseFloat(primeiroconj.join()) + parseFloat(segundoconj.join());
+            
+            visualizacao = resutado;
+            imprimir();
+            break;
+    }
 }
 
-var pontodivisao;
+var equacao;
 function divisao() {
-    pontocontrol = 1;
-    pontodivisao = 1;
-    sinal = " / "
-    visualizacao = primeiroconj.join("") + primeiroconj.push(" / ");
-    resu_visu.innerHTML = visualizacao ;
+    if (pontocontrol === 0) {
+        pontocontrol = 1;
+        equacao = 1;
+        sinal = " / "
+        visualizacao = primeiroconj.join("") + sinal;
+        resu_visu.innerHTML = visualizacao;
+    } else {
+        alert("Até o momento a calculadora não realiza mais de uma operação \nRecarregue a página ou limpe a calculadora")
+    }
 }
 
 
+function multiplicacao() {
+    if (pontocontrol === 0) {
+        pontocontrol = 1;
+        equacao = 2;
+        sinal = " X ";
+        visualizacao = primeiroconj.join("") + sinal;
+        resu_visu.innerHTML = visualizacao;
+    } else {
+        alert("Até o momento a calculadora não realiza mais de uma operação \nRecarregue a página ou limpe a calculadora")
+    }
+}
+
+
+function subtracao() {
+    if (pontocontrol === 0) {
+        pontocontrol = 1;
+        equacao = 3;
+        sinal = " - ";
+        visualizacao = primeiroconj.join("") + sinal;
+        resu_visu.innerHTML = visualizacao;
+    } else {
+        alert("Até o momento a calculadora não realiza mais de uma operação \nRecarregue a página ou limpe a calculadora")
+    }
+}
+
+
+function adicao() {
+    if (pontocontrol === 0) {
+        pontocontrol = 1;
+        equacao = 4;
+        sinal = " + ";
+        visualizacao = primeiroconj.join("") + sinal;
+        resu_visu.innerHTML = visualizacao;
+    } else {
+        alert("Até o momento a calculadora não realiza mais de uma operação \nRecarregue a página ou limpe a calculadora")
+    }
+}
+
+
+function ponto() {
+    if (pontocontrol === 1) {
+        segundoconj.push(".");
+
+    } else {
+        primeiroconj.push(".");
+
+    }
+    visualizacao = primeiroconj.join("") + sinal + segundoconj.join("");
+    imprimir();
+}
 
 
 function num0() {
 
     if (pontocontrol === 1) {
         segundoconj.push("0");
-        visualizacao = segundoconj.join("");
-        imprimir();
+
     } else {
         primeiroconj.push("0");
-        visualizacao = primeiroconj.join("");
-        imprimir();
-    }
-    
 
+    }
+    visualizacao = primeiroconj.join("") + sinal + segundoconj.join("");
+    imprimir();
 }
 function num1() {
     if (pontocontrol === 1) {
         segundoconj.push("1");
-        visualizacao =   segundoconj.join("");
-        imprimir();
     } else {
         primeiroconj.push("1");
-        visualizacao = primeiroconj.join("");
-        imprimir();
     }
 
+    visualizacao = primeiroconj.join("") + sinal + segundoconj.join("");
+    imprimir();
 }
 function num2() {
     if (pontocontrol === 1) {
         segundoconj.push("2");
-        visualizacao =   segundoconj.join("");
+
         imprimir();
     } else {
         primeiroconj.push("2");
-        visualizacao = primeiroconj.join("");
+
         imprimir();
     }
+    visualizacao = primeiroconj.join("") + sinal + segundoconj.join("");
+    imprimir();
 }
 function num3() {
     if (pontocontrol === 1) {
         segundoconj.push("3");
-        visualizacao =   segundoconj.join("");
+
         imprimir();
     } else {
         primeiroconj.push("3");
-        visualizacao = primeiroconj.join("");
+
         imprimir();
     }
+    visualizacao = primeiroconj.join("") + sinal + segundoconj.join("");
+    imprimir();
 }
 function num4() {
     if (pontocontrol === 1) {
         segundoconj.push("4");
-        visualizacao =   segundoconj.join("");
+
         imprimir();
     } else {
         primeiroconj.push("4");
-        visualizacao = primeiroconj.join("");
+
         imprimir();
     }
+    visualizacao = primeiroconj.join("") + sinal + segundoconj.join("");
+    imprimir();
 }
 function num5() {
     if (pontocontrol === 1) {
         segundoconj.push("5");
-        visualizacao =  segundoconj.join("");
+
         imprimir();
     } else {
         primeiroconj.push("5");
-        visualizacao = primeiroconj.join("");
+
         imprimir();
     }
+    visualizacao = primeiroconj.join("") + sinal + segundoconj.join("");
+    imprimir();
 }
 function num6() {
     if (pontocontrol === 1) {
         segundoconj.push("6");
-        visualizacao =   segundoconj.join("");
+
         imprimir();
     } else {
         primeiroconj.push("6");
-        visualizacao = primeiroconj.join("");
+
         imprimir();
     }
+    visualizacao = primeiroconj.join("") + sinal + segundoconj.join("");
+    imprimir();
 }
 function num7() {
     if (pontocontrol === 1) {
         segundoconj.push("7");
-        visualizacao =   segundoconj.join("");
+
         imprimir();
     } else {
         primeiroconj.push("7");
-        visualizacao = primeiroconj.join("");
+
         imprimir();
     }
+    visualizacao = primeiroconj.join("") + sinal + segundoconj.join("");
+    imprimir();
 }
 function num8() {
     if (pontocontrol === 1) {
         segundoconj.push("8");
-        visualizacao =   segundoconj.join("");
+
         imprimir();
     } else {
         primeiroconj.push("8");
-        visualizacao = primeiroconj.join("");
+
         imprimir();
     }
+    visualizacao = primeiroconj.join("") + sinal + segundoconj.join("");
+    imprimir();
 }
 
 function num9() {
     if (pontocontrol === 1) {
         segundoconj.push("9");
-        visualizacao =   segundoconj.join("");
+
         imprimir();
     } else {
         primeiroconj.push("9");
-        visualizacao = primeiroconj.join("");
+
         imprimir();
     }
+    visualizacao = primeiroconj.join("") + sinal + segundoconj.join("");
+    imprimir();
 }
+
 
 function reset() {
     primeiroconj = [];
     segundoconj = [];
+
     pontocontrol = 0;
 
-    var resu = primeiroconj.join("");
-    resu_visu.innerHTML =  "0";
+    equacao = 0;
+    equacao = 0;
+
+    sinal = [];
+    resu_visu.innerHTML = "0";
 
 }
 
