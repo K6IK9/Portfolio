84cc43ecc49ecb4979422a85c738a9f8cbaf4b25
Kaike Matos <kaikematos50@gmail.com>
2024-01-14 19:35:56 -0400

    Atualização de organização e efeitos
    código organizado novamente, e efeitos ajustados para certos dispositivos

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function noturno() {
     var hrefstyle = linkstyle.getAttribute("href");
 
     if(hrefstyle === "css/style.css"){
-        linkstyle.setAttribute("href", "css/style_Escuro.css");
+        linkstyle.setAttribute("href", "css/style_escuro.css");
     }else{
         linkstyle.setAttribute("href", "css/style.css");
     }
